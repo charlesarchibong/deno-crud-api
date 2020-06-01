d695ad1fba2416fa1de4c18a4db41de8d6151490
Charles Archibong <charlesarchibong10@gmail.com>
2020-06-01 02:45:58 +0100

    fixed bugs

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -6,30 +6,64 @@ export default {
     context.response.body = await User.getAll();
   },
   async show(context: any) {
-    context.response.status = 200;
     const id = context.params.id;
-    context.response.body = await User.get(id);
+    const user = await User.get(id);
+    if (user != null) {
+      context.response.status = 200;
+      context.response.body = user;
+    } else {
+      context.response.status = 400;
+      context.response.body = { error: "No user record found" };
+    }
   },
   async store(context: any) {
-    const { value } = await context.request.body();
-    const inserted = await User.insert(value);
-    context.response.status = 201;
-    context.response.body = { success: true, inserted };
+    if (context.request.hasBody) {
+      const { value } = await context.request.body();
+      const inserted = await User.insert(value);
+      context.response.status = 201;
+      context.response.body = { success: true, inserted };
+    } else {
+      context.response.status = 400;
+      context.response.body = { error: "Please provide the required data" };
+    }
   },
   async update(context: any) {
     const id = context.params.id;
     console.log(id);
+    const user = await User.get(id);
+    if (user == null) {
+      context.response.status = 400;
+      context.response.body = { error: "User record was not found" };
+      return;
+    }
     const { value } = await context.request.body();
     const upsertedId = await User.update(id, value);
     console.log(upsertedId);
     if (upsertedId != null) {
+      context.response.status = 200;
       context.response.body = { success: true, upsertedId };
     } else {
+      context.response.status = 400;
       context.response.body = { error: "No user found with this id" };
     }
   },
   async destroy(context: any) {
     const id = context.params.id;
-    context.response.body = User.delete(id);
+    const user = await User.get(id);
+    if (user == null) {
+      context.response.status = 400;
+      context.response.body = { error: "User record was not found" };
+      return;
+    }
+    const deleted = await User.delete(id);
+    if (deleted != null) {
+      context.response.status = 200;
+      context.response.body = {
+        message: "User record was deleted successfully",
+      };
+    } else {
+      context.response.status = 400;
+      context.response.body = { error: "User record was not deleted" };
+    }
   },
 };
