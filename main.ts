4a3487ff6709f0e44c86177c85bf7a18d4d1a4b4
Charles Archibong <charlesarchibong10@gmail.com>
2020-06-01 01:58:50 +0100

    added utils package

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 import router from "./routes/routes.ts";
-import notFound from "./404.ts";
+import notFound from "./utils/404.ts";
 const app = new Application();
 
 const env = config();
