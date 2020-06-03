5f13f21699e46f65b2f2615c124a49abcd8428c2
Charles Archibong <charlesarchibong10@gmail.com>
2020-06-03 20:26:52 +0100

    resolving mongo connectivity errors

--- main.ts
+++ main.ts
@@ -1,16 +1,12 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
-
+import * as flags from "https://deno.land/std/flags/mod.ts";
 import router from "./routes/routes.ts";
 import notFound from "./utils/404.ts";
 const app = new Application();
 
 const env = config();
 const PORT = +env.PORT || 3000;
-// router.get('get',(context){
-//    context.
-
-// })
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use(notFound);
